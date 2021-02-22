4e829c4dde54823d823fae09aec6fb4af983fb46
ariefsetyan <ariefsetyanugraha@ymail.com>
2021-02-22 22:29:28 +0700

    splashSrean & ProgressBar

--- app/src/main/java/com/example/ruangbahasa/MainActivity.java
+++ app/src/main/java/com/example/ruangbahasa/MainActivity.java
@@ -2,13 +2,78 @@ package com.example.ruangbahasa;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
+    private WebView web;
+    private ProgressBar progressBar;
+    private String URL = "http://ruang-bahasa.my.id/";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        web = (WebView) findViewById(R.id.webview);
+        progressBar = findViewById(R.id.progresBar);
+
+        Setting();
+        Loadding();
+
+    }
+
+    private void Setting(){
+        WebSettings web_settings = web.getSettings();
+        web_settings.setJavaScriptEnabled(true);
+        web_settings.setAllowContentAccess(true);
+        web_settings.setUseWideViewPort(true);
+        web_settings.setLoadsImagesAutomatically(true);
+        web_settings.setCacheMode(WebSettings.LOAD_NO_CACHE);
+        web_settings.setRenderPriority(WebSettings.RenderPriority.HIGH);
+        web_settings.setEnableSmoothTransition(true);
+        web_settings.setDomStorageEnabled(true);
+    }
+
+    private void Loadding(){
+        if(Build.VERSION.SDK_INT >= 19){
+            web.setLayerType(View.LAYER_TYPE_HARDWARE, null);
+        }else{
+            web.setLayerType(View.LAYER_TYPE_SOFTWARE, null);
+        }
+        web.setWebChromeClient(new WebChromeClient(){
+            public void onProgressChanged(WebView view, int newProgress){
+                progressBar.setVisibility(View.VISIBLE);
+                progressBar.setProgress(newProgress);
+                if(newProgress == 100){
+                    //ProgressBar akan Menghilang setelah Valuenya mencapat 100%
+                    progressBar.setVisibility(View.GONE);
+                }
+                super.onProgressChanged(view, newProgress);
+            }
+        });
+        web.setWebViewClient(new WebViewClient(){
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                view.loadUrl(request.toString());
+                return true;
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                //ProgressBar akan Menghilang setelah Halaman Selesai Dimuat
+                super.onPageFinished(view, url);
+                progressBar.setVisibility(View.GONE);
+            }
+        });
+        web.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+        web.loadUrl(URL);
     }
 }
